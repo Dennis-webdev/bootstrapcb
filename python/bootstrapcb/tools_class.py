5cd14b9e70122cc6bf924f1ce12019f0e878308d
Dennis-webdev <dennisrichter.business@gmail.com>
2021-02-21 01:54:31 +0100

    wrote some helper functions

--- python/bootstrapcb/tools_class.py
+++ python/bootstrapcb/tools_class.py
@@ -11,6 +11,7 @@ from scipy.integrate import quad
 
 class cb_class:
     # Data
+    _file_name = "dict.tmp"
     _xdata = None
     _ydata = None
     xs = None
@@ -18,6 +19,7 @@ class cb_class:
     # Model
     _f = None
     _p0 = None
+    _p = None
     _pdf = None
     _cdf = None
     # MLE
@@ -28,7 +30,11 @@ class cb_class:
     _parametric = True
     _x_samples = None
     _y_samples = None
+    _n = 50
     _theta_samples = None
+    _B = 100
+    _C = None
+    _L = None
 
     def __init__(self, xdata, ydata, f, p0=None, pdf=None, cdf=None):
         # get data
@@ -41,7 +47,10 @@ class cb_class:
                 self.ys.append(ydata[i][j])
         # get parameter guess
         if not p0:
-            p0 = [0]*self.listarg_len(self._f)
+            p = self.listarg_len(self._f)
+            p0 = [0]*p
+        else:
+            p = len(p0)
         # get normal distribution if neither is given
         if not pdf and not cdf:
             def pdf(x,y,theta): 
@@ -57,6 +66,7 @@ class cb_class:
             def cdf(x,y,theta): 
                 return quad(lambda var: self._pdf(x,var,theta), -np.inf, y)
         self._f = f
+        self._p = p
         self._p0 = p0
         self._pdf = pdf
         self._cdf = cdf
@@ -101,17 +111,17 @@ class cb_class:
             else:
                 return count
 
-    def get_bs_samples(self, B=100, n=50):
+    def get_bs_samples(self):
         try: 
             print("Importing samples")
-            file_path = os.getcwd()+"/dict.tmp"
+            file_path = os.getcwd()+"/"+self._file_name
             file = open(file_path)
             dict = file.read()
             dict = eval(dict) 
             # Check
             assert dict['parametric'] == self._parametric 
-            assert len(dict['y_samples']) == B
-            assert len(dict['y_samples'][0]) == n
+            assert len(dict['y_samples']) == self._B
+            assert len(dict['y_samples'][0]) == self._n
             assert len(dict['theta_samples'][0]) == len(self._p0)
             # Import
             self._x_samples = dict['x_samples']
@@ -123,10 +133,10 @@ class cb_class:
             print("Couldn't import samples, now generating...")
             # Generate
             X, Y, Theta = [], [], []
-            for i in range(B):
+            for i in range(self._B):
                 x_sample = []
                 y_sample = []
-                for _ in range(n): 
+                for _ in range(self._n): 
                     r = np.random.uniform(0, 1)
                     index = np.random.choice(range(len(self._xdata)))
                     x_sample.append(self._xdata[index])
@@ -138,7 +148,7 @@ class cb_class:
                 X.append(x_sample)
                 Y.append(y_sample)
                 Theta.append(theta_sample)
-                print("Generate bootstrap samples: {0}%".format( int(100*(i+1)/B) ), end="\r")
+                print("Generate bootstrap samples: {0}%".format( int(100*(i+1)/self._B) ), end="\r")
             self._x_samples, self._y_samples, self._theta_samples = X, Y, Theta
             dict = {
                 'parametric': self._parametric,
@@ -146,13 +156,24 @@ class cb_class:
                 'y_samples': self._y_samples,
                 'theta_samples': self._theta_samples
             }
-            file_path = os.getcwd()+"/dict.tmp"
+            file_path = os.getcwd()+"/"+self._file_name
             file = open(file_path,"w")
             file.write( str(dict) ) 
             file.close() 
             print("\n")
             print("Successfully imported samples")
         return self._x_samples, self._y_samples, self._theta_samples
+
+    def set_sample_size(self, B=100, n=50):
+        self._B = B
+        self._n = n
+    
+    def clear_file(self):
+        file_path = os.getcwd()+"/"+self._file_name
+        file = open(file_path,"w")
+        file.write( "" ) 
+        file.close() 
+        print("File cleared")
 
     def mle(self, x):
         try:
@@ -164,13 +185,52 @@ class cb_class:
             exit(1)
         return [self._f(x_i, self._opt) for x_i in x], self._V
 
-    def conf_band_delta(self, x, q):
+    def _check_mle_and_samples(self):
         # get MLE
         if not self._opt:
             self.mle()
         # get BS samples
         if not self._theta_samples:
             self.get_bs_samples()
+
+    def _check_c_values(self):
+        if not self._C:
+            x_samples = self._x_samples.copy()
+            y_samples = self._y_samples.copy()
+            theta_samples = self._theta_samples.copy()
+            # jth GoF test statistic
+            C = []
+            for theta in theta_samples:
+                c_sample = np.subtract(self._opt, theta).T @ self._I @ np.subtract(self._opt, theta)
+                C.append(c_sample)
+            # cholesky
+            L = np.linalg.cholesky(self._V)
+            # order test values
+            B = len(self._theta_samples)
+            for i in range(B):
+                for j in range(i+1,B):
+                    if C[j] > C[i]: 
+                        tmpX = x_samples[i] 
+                        x_samples[i] = x_samples[j]
+                        x_samples[j] = tmpX
+                        tmpY = y_samples[i] 
+                        y_samples[i] = y_samples[j]
+                        y_samples[j] = tmpY
+                        tmpTheta = theta_samples[i] 
+                        theta_samples[i] = theta_samples[j]
+                        theta_samples[j] = tmpTheta
+                        tmpC = C[i] 
+                        C[i] = C[j]
+                        C[j] = tmpC
+            self._x_samples = x_samples
+            self._y_samples = y_samples
+            self._theta_samples = theta_samples
+            self._C = C
+            self._L = L
+
+    def conf_band_delta(self, x, q):
+        self._check_mle_and_samples()
+        # delta method
         confBand_L, confBand_U = [None]*len(x), [None]*len(x)
         for i in range(len(x)):
             dg = nd.Gradient(lambda var: self._f(x[i], var))
@@ -181,33 +241,11 @@ class cb_class:
         return confBand_L, confBand_U
 
     def conf_band_bs_ralpha(self, x, q):
-        # get MLE
-        if not self._opt:
-            self.mle()
-        p = len(self._opt)
-        # get BS samples
-        if not self._theta_samples:
-            self.get_bs_samples()
-        B = len(self._theta_samples)
-        # jth GoF test statistic
-        C = []
-        theta_samples = self._theta_samples.copy()
-        for theta in theta_samples:
-            c_sample = np.subtract(self._opt, theta).T @ self._I @ np.subtract(self._opt, theta)
-            C.append(c_sample)
-        # order test values
-        for i in range(B):
-            for j in range(i+1,B):
-                if C[j] > C[i]: 
-                    tmpC = C[i] 
-                    C[i] = C[j]
-                    C[j] = tmpC
-                    tmpTheta = self._theta_samples[i] 
-                    theta_samples[i] = theta_samples[j]
-                    theta_samples[j] = tmpTheta
+        self._check_mle_and_samples()
+        self._check_c_values()
+        c_critval = np.percentile(self._C, q) 
         # rounded integer subscript
-        c_critval = np.percentile(C, q) 
-        R_alpha = [theta_samples[i] for i in range(B) if C[i] < c_critval]
+        R_alpha = [self._theta_samples[i] for i in range(self._B) if self._C[i] < c_critval]
         confBand_L, confBand_U = [None]*len(x), [None]*len(x)
         for theta in R_alpha:
             for i in range(len(x)):
@@ -219,40 +257,17 @@ class cb_class:
         return confBand_L, confBand_U
 
     def conf_band_bs_dralpha(self, x, q):
-        # get MLE
-        if not self._opt:
-            self.mle()
-        p = len(self._opt)
-        # get BS samples
-        if not self._theta_samples:
-            self.get_bs_samples()
-        B = len(self._theta_samples)
-        # jth GoF test statistic
-        L = np.linalg.cholesky(self._V)
-        C = []
-        theta_samples = self._theta_samples.copy()
-        for theta in theta_samples:
-            c_sample = np.subtract(self._opt, theta).T @ self._I @ np.subtract(self._opt, theta)
-            C.append(c_sample)
-        # order test values
-        for i in range(B):
-            for j in range(i+1,B):
-                if C[j] > C[i]: 
-                    tmpC = C[i] 
-                    C[i] = C[j]
-                    C[j] = tmpC
-                    tmpTheta = theta_samples[i] 
-                    theta_samples[i] = theta_samples[j]
-                    theta_samples[j] = tmpTheta
-        # rounded integer subscript
-        c_critval = np.percentile(C, q) 
+        self._check_mle_and_samples()
+        self._check_c_values()
+        c_critval = np.percentile(self._C, q) 
+        # random points on the surface
         dR_alpha = []
         for j in range(1000):
-            z = np.random.normal(0, 1, p)
-            z_sum = np.sum([z[j]**2 for j in range(p)])
+            z = np.random.normal(0, 1, self._p)
+            z_sum = np.sum([z[j]**2 for j in range(self._p)])
             z_sum = np.sqrt(z_sum)
-            Z = [np.sqrt(c_critval) * z[i] / z_sum for i in range(p)]
-            W = L @ Z
+            Z = [np.sqrt(c_critval) * z[i] / z_sum for i in range(self._p)]
+            W = self._L @ Z
             dR_alpha.append(np.add(W, self._opt))
         confBand_L, confBand_U = [None]*len(x), [None]*len(x)
         for theta in dR_alpha:
@@ -265,44 +280,21 @@ class cb_class:
         return confBand_L, confBand_U
 
     def conf_band_approx_dralpha(self, x, q):
-        # get MLE
-        if not self._opt:
-            self.mle()
-        p = len(self._opt)
-        # get BS samples
-        if not self._theta_samples:
-            self.get_bs_samples()
-        B = len(self._theta_samples)
-        # jth GoF test statistic
-        L = np.linalg.cholesky(self._V)
-        C = []
-        theta_samples = self._theta_samples.copy()
-        for theta in theta_samples:
-            c_sample = np.subtract(self._opt, theta).T @ self._I @ np.subtract(self._opt, theta)
-            C.append(c_sample)
-        # order test values
-        for i in range(B):
-            for j in range(i+1,B):
-                if C[j] > C[i]: 
-                    tmpC = C[i] 
-                    C[i] = C[j]
-                    C[j] = tmpC
-                    tmpTheta = theta_samples[i] 
-                    theta_samples[i] = theta_samples[j]
-                    theta_samples[j] = tmpTheta
-        # rounded integer subscript
-        c_critval = np.percentile(C, q) 
+        self._check_mle_and_samples()
+        self._check_c_values()
+        c_critval = np.percentile(self._C, q) 
+        # search restricted to points on the surface
         def _restircted_search(z,x):
-            z_sum = np.sum([z[j]**2 for j in range(p)])
+            z_sum = np.sum([z[j]**2 for j in range(self._p)])
             z_sum = np.sqrt(z_sum)
-            Z = [np.sqrt(c_critval) * z[i] / z_sum for i in range(p)]
-            W = L @ Z
+            Z = [np.sqrt(c_critval) * z[i] / z_sum for i in range(self._p)]
+            W = self._L @ Z
             theta = np.add(W, self._opt)
             return self._f(x, theta)
         confBand_L, confBand_U = [None]*len(x), [None]*len(x)
         for i in range(len(x)):
-            min = minimize(lambda var: _restircted_search(var,x[i]), [0]*p, method='Nelder-Mead').x.tolist()
+            min = minimize(lambda var: _restircted_search(var,x[i]), [0]*self._p, method='Nelder-Mead').x.tolist()
             confBand_L[i] = _restircted_search(min,x[i])
-            max = minimize(lambda var: -_restircted_search(var,x[i]), [0]*p, method='Nelder-Mead').x.tolist()
+            max = minimize(lambda var: -_restircted_search(var,x[i]), [0]*self._p, method='Nelder-Mead').x.tolist()
             confBand_U[i] = _restircted_search(max,x[i])
         return confBand_L, confBand_U
